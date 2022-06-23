2e0aa13901c490d653a6c215520cc4405b201bb0
tareita <tareitaa@outlook.com>
2022-06-23 18:08:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hgi</h1>
+      <p>qwertyuiopasdfghjklzxcvbnmn</p>
+      <img
+        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+        width="100px"
+        height="100px"
+      ></img>
     </div>
   );
 }
