cfd7e192b798e64c788f5f16a7116c73fa52d549
tareita <tareitaa@outlook.com>
2022-06-24 15:50:43 +0100

    decomposing code in react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import "./App.css";
+import Listing from "./Listing";
 
 function App() {
   return (
     <div className="App">
-      <h1>hgi</h1>
-      <p>qwertyuiopasdfghjklzxcvbnmn</p>
-      <img
-        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-        width="100px"
-        height="100px"
-      ></img>
+      <Listing />
+      <Listing />
+      <Listing />
     </div>
   );
 }
---
+++ src/Listing.js
@@ -0,0 +1,14 @@
+function Listing() {
+  return (
+    <div>
+      <h1>hgi</h1>
+      <p>qwertyuiopasdfghjklzxcvbnmn</p>
+      <img
+        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+        width="100px"
+        height="100px"
+      ></img>
+    </div>
+  );
+}
+export default Listing;
